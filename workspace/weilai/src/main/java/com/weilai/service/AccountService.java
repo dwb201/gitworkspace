cc5e10f06f7796ebb9c9e4886e0699477b493035
dwb <dwb201@hotmail.com>
2017-02-25 22:18:05 +0900

    add

--- workspace/weilai/src/main/java/com/weilai/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.weilai.service;
-
-import com.weilai.pojo.User;
-
-public abstract interface AccountService
-{
-  public abstract User getUserByEmailId(String emailkey);
-  public abstract int insertUser(User user);
-}
